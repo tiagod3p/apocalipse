6197abd0d6efb9bdd5f132e8dd7ab0159736abe1
BMS <saymon.titi@gmail.com>
2020-08-20 21:20:26 -0300

    feat: ensure SurvivorController returns 400 if no age is provided

--- src/presentation/controllers/survivor.ts
+++ src/presentation/controllers/survivor.ts
@@ -1,9 +1,16 @@
 export class SurvivorController {
   handle (httpRequest: any): any {
-    if (!httpRequest.name) {
+    if (!httpRequest.body.name) {
       return {
         statusCode: 400,
         body: new Error('Missing param: name')
+      }
+    }
+
+    if (!httpRequest.body.age) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: age')
       }
     }
   }
