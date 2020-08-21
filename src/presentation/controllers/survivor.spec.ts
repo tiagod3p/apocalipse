8e79b50c6bf59fdb5c8410af8686c07a64e7cc19
BMS <saymon.titi@gmail.com>
2020-08-20 21:21:05 -0300

    test: ensure SurvivorController returns 400 if no age is provided

--- src/presentation/controllers/survivor.spec.ts
+++ src/presentation/controllers/survivor.spec.ts
@@ -17,4 +17,20 @@ describe('Survivor Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
+  test('Should return 400 if no age is provided', () => {
+    const sut = new SurvivorController()
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        gender: 'any_gender',
+        location: 'any location',
+        inventory: 'any_inventory'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: age'))
+  })
 })
